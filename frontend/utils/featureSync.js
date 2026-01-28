7f69d297aa0cefa4cebb661bfd1e11c5bc0e69a5
swaffX <kxrk0@github.com>
2026-01-28 18:30:01 +0300

    xddxx

--- frontend/utils/featureSync.js
+++ frontend/utils/featureSync.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://neuroviabot.xyz';
 
 // Frontend feature list (hardcoded for comparison)
 const FRONTEND_FEATURES = [
